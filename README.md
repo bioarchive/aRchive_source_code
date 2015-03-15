36604dc7d5fff51aecdefc07bb9fe3f5c1a224d5
Nitesh Turaga <nitesh.turaga@gmail.com>
2015-03-14 23:28:35 -0400

    added functions to download and archive local repos

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 *aRchive* source code
 ======================
 
-Here lives the source code for *aRchive*
+Here lives the source code for *aRchive*.
 
 ###Description
 
-*aRchive* stores versions of BioConductor packages to promote interoperability between Galaxy and Bioconductor. 
+*aRchive* stores versions of BioConductor packages to promote interoperability between Galaxy and Bioconductor. The main idea is to allow
+users access to all the versions of current BioConductor packages.
--- aRchive.py
+++ aRchive.py
@@ -1,3 +1,38 @@
 #!/usr/bin/env python
 
-import os
+import os,sys,svn
+import svn.local,svn.remote
+import pprint
+
+
+# Download the Bioconductor Repo
+def downloadMainBiocRepo(path):
+    """
+    Check out the main repository from Bioconductor giving 
+    user-name: readonly; 
+    password: readonly;
+    """
+    os.chdir(path)
+    if os.path.isdir(".svn"):
+        os.system("svn update")
+        print "SVN repo updated"
+    else:        
+        mainRepo = svn.remote.RemoteClient("https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_0/madman/Rpacks/")
+        pprint.pprint(mainRepo.info())
+        mainRepo.checkout(path)
+    return "Bioconductor Release version repository completely downloaded"
+
+
+def archiveLocalRepo(path):
+    # Check local repository for current revision number
+    localRepo = svn.local.LocalClient('/Users/nturaga/Documents/Bioconductor-Rpacks/Rpacks')
+    pprint.pprint(localRepo.info())
+
+    # Print the version number
+    print localRepo.info()['commit#revision']
+
+
+if __name__ == "__main__":
+    downloadMainBiocRepo('/Users/nturaga/Documents/Bioconductor-Rpacks/Rpacks')
+    archiveLocalRepo('/Users/nturaga/Documents/Bioconductor-Rpacks/Rpacks')
+    
