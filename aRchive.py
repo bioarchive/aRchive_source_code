cd660cd9d832ecb6fdb7aaa16fdecdbe7d10edb0
Nitesh Turaga <nitesh.turaga@gmail.com>
2015-03-25 08:08:57 -0400

    8:00am

--- aRchive.py
+++ aRchive.py
@@ -44,6 +44,7 @@ def makeVersion(folder):
     # Get history of the SVN repo and get all revert IDs
     history = subprocess.check_output(['svn','log'])
     revert_ids = [line[0:line.find(" |")] for line in history.splitlines() if re.match("^r[0-9]",line)]
+    print revert_ids
     # Get the version number of the Bioconductor package from DESCRIPTION file in SVN repo
     with open(os.path.join(folder,"DESCRIPTION")) as fp:
         latest_version = str([line[8:].strip() for i,line in enumerate(fp) if re.match("^Version: [0-9]",line)][0])
@@ -58,29 +59,35 @@ def makeVersion(folder):
             curr_version = str([line[8:].strip() for i,line in enumerate(fp2) if re.match("^Version: [0-9]",line)][0])
         print "\n\n\n Current revision is %s at %s version \n\n"% (str(localRepo.info()['commit#revision']),curr_version)
 
-        if curr_version==latest_version:
-            print curr_version==latest_version
-            latest_version = curr_version
+        if curr_version!=latest_version:
+            print folder 
+            # Create new directory with version number as "-version" extention
+            directory = os.path.split(folder)[-1]
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+                
+                print "HERE"
+
             # TODO: LOGIC ERROR
         else:
             print "Save to another folder"
             # os.system("tar -zxvf .")
-        if id=="r100643":break
+        if id=="r101096":break
     # Return to most recent update
     update_cmd = subprocess.Popen(["svn","update"],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     update_out,update_err=update_cmd.communicate()
     return 
 
 
-def archiveLocalRepo(path):
+def archiveLocalRepo(archive_path,bioc_path):
     # Check local repository for current revision number
-    localRepo = svn.local.LocalClient(path)
+    localRepo = svn.local.LocalClient(bioc_path)
     # pprint.pprint(localRepo.info())
     # Print the version number
     # print "Printing Repository revision number: ", localRepo.info()['commit#revision']
-    rpacks = os.listdir(path)
+    rpacks = os.listdir(bioc_path)
     testPack = rpacks[rpacks.index('DESeq2')]
-    makeVersion(os.path.join(path,testPack))
+    makeVersion(archive_path,os.path.join(bioc_path,testPack))
     # for folder in rpacks:
         # if os.path.isdir(os.path.join(path,folder)) and not folder.startswith("."):
             # makeVersion(os.path.join(path,folder))
@@ -95,16 +102,19 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(add_help=True, description="Create Bioconductor archive for all packages, \
         and version them based on commit history. If the command is rerun, it should automatically add to an \
         existing archive.")
-    parser.add_argument("-dir",dest="archive_directory",help="Name of directory where aRchive.py should work",type=str)
-
-    if len(sys.argv) <= 1:
+    parser.add_argument("-bioconductor_dir",dest="bioconductor_dir",help="Name of directory where aRchive.py should work i.e the Bioconductor clone repository",type=str)
+    parser.add_argument("-archive_dir",dest="archive_dir",help="Directory name to host all versions of all bioconductor packages. This is where the archive lives",type=str)
+    if len(sys.argv) <= 2:
         parser.print_usage()
         sys.exit(1)
     else:
         args = parser.parse_args()
 
-    ARCHIVE_DIR = os.path.abspath(args.archive_directory)
-    print "aRchive is being run in %s " % ARCHIVE_DIR
-    downloadMainBiocRepo(ARCHIVE_DIR)
-    # archiveLocalRepo(ARCHIVE_DIR)
+
+    BIOCONDUCTOR_DIR = os.path.abspath(args.bioconductor_dir)
+    ARCHIVE_DIR = os.path.abspath(args.archive_dir)
+    print "aRchive is being run in %s " % BIOCONDUCTOR_DIR
+    print "aRchive is being stored in %s" % ARCHIVE_DIR
+    downloadMainBiocRepo(BIOCONDUCTOR_DIR)
+    archiveLocalRepo(BIOCONDUCTOR_DIR,ARCHIVE_DIR)
     
